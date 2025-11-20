d1941a8e6388e6dc4420a882a1cb4198ce56341b
Alyssa Signorelli <signorelliag@gmail.com>
2025-11-20 19:09:51 +0000

    added dropdown and fixed nav bar on all pages

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -28,6 +28,26 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.add("page-loaded");
 });
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 // WORD PAGE
 //adapted on this tutorial: https://www.tutorialpedia.org/blog/word-cloud-html-css/
 //and then edited by Alyssa
